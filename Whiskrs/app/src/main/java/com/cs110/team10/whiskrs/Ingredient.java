3d953c49db016604551a65623e55ab2c7e1b4347
John Pallag <john.pallag@webble.com>
2016-01-20 21:25:44 -0800

    Actually pushing new application, last one was not correct

--- Whiskrs/app/src/main/java/com/cs110/team10/whiskrs/Ingredient.java
+++ Whiskrs/app/src/main/java/com/cs110/team10/whiskrs/Ingredient.java
@@ -1,7 +1,11 @@
 package com.cs110.team10.whiskrs;
 
+import android.media.Image;
+
 /**
  * Created by John on 1/20/2016.
  */
 public class Ingredient {
+    public String name;
+    public Image picture;
 }
---
+++ Whiskrs/app/src/main/java/com/cs110/team10/whiskrs/IngredientsInput.java
@@ -0,0 +1,48 @@
+package com.cs110.team10.whiskrs;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.goebl.david.Webb;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class IngredientsInput extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ingredients_input);
+
+
+        Button search = (Button)findViewById(R.id.searchIngredients);
+        search.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText in = (EditText)findViewById(R.id.editText);
+                String text = in.getText().toString();
+                clearInput(in);
+                TextView out = (TextView)findViewById(R.id.output);
+                out.setText(text);
+                /*Webb webb = Webb.create();
+                JSONObject result = webb
+                        .get("http://maps.googleapis.com/maps/api/directions/json")
+                        .ensureSuccess()
+                        .asJsonObject()
+                        .getBody();*/
+            }
+        });
+    }
+
+    private void clearInput(EditText in){
+        in.setText("");
+    }
+}
