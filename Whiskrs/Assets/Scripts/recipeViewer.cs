2fbf1e85179e9d82259d791186e0bab2f8f75fba
John Pallag <john.pallag@webble.com>
2016-02-21 14:15:37 -0800

    Added scrolling for ingredients in recipe and directions.

--- Whiskrs/Assets/Scripts/recipeViewer.cs
+++ Whiskrs/Assets/Scripts/recipeViewer.cs
@@ -24,7 +24,15 @@ public class recipeViewer : MonoBehaviour {
         main.Instance.closePanel(favoritesPanel.Instance.gameObject);
         if (result.img != null) setImage(result.img);
         ingredientsObject.text = string.Join(",", result.ingredients.ToArray());
+        int ingLines = (int)((ingredientsObject.text.Length * 25) / Screen.width);
+        RectTransform ingrt = ingredientsObject.gameObject.GetComponent<RectTransform>();
+        ingrt.sizeDelta = new Vector2(ingrt.sizeDelta.x, ingLines * 40);
+
         directionsObject.text = result.directions;
+        int dirLines = (int)((directionsObject.text.Length * 25) / Screen.width);
+        RectTransform dirrt = directionsObject.gameObject.GetComponent<RectTransform>();
+        dirrt.sizeDelta = new Vector2(dirrt.sizeDelta.x, dirLines * 40);
+
         nameObject.text = result.name;
     }
 }
