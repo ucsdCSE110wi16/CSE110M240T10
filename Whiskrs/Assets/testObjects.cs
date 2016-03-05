3adb9be331c038a3cf1d0aa597c045eaf856b8f8
John Pallag <john.pallag@webble.com>
2016-03-05 07:32:54 -0800

    Whiskr Testing Updates: -Finished defining buttons to be tested for Scenarios -Changed the play sound button to be different from the mute sound button

--- Whiskrs/Assets/testObjects.cs
+++ Whiskrs/Assets/testObjects.cs
@@ -7,13 +7,27 @@ public class testObjects : MonoBehaviour {
     public static testObjects e;
     public static bool runTests;
 
-    /* Buttons */
+    /* Buttons on Menu*/
     public Button openPantryButton;
     public Button openRecipesButton;
+    public Button openFavoritesButton;
+    /* Go Home Buttons */
     public Button openHomeButton_FromPantry;
     public Button openHomeButton_FromRecipes;
     public Button openHomeButton_FromRecipeViewer;
     public Button openHomeButton_FromFavorites;
+    /* Go To Pantry Buttons */
+    public Button openPantryButton_FromRecipes;
+    public Button openPantryButton_FromSearchIngredients;
+    /* Go To Recipes Buttons */
+    public Button openRecipesButton_FromPantry;
+    public Button openRecipesButton_FromRecipe;
+    public Button openRecipesButton_FromFavorites;
+    public Button openRecipesButton_FromFilters;
+    /* Go To Filters Buttons */
+    public Button openFiltersButton_FromRecipes;
+    /* Go To Search Ingredients */
+    public Button openSearchIngredients_FromPantry;
     /* Panels */
     public GameObject pantryPanel;
     public GameObject homePanel;
